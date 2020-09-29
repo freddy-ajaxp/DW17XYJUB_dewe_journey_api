59037e9a4b3515e23a3c09fdd88c20bfaa7d4471
freddy-ajaxp <67192469+freddy-ajaxp@users.noreply.github.com>
2020-09-29 19:15:25 +0700

    Add files via upload

--- models/userx.js
+++ models/userx.js
@@ -4,13 +4,14 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Userx extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
-      // define association here
+      Userx.hasOne(models.Tripx, {
+        foreignKey: "userId",
+      }),
+      Userx.hasOne(models.Bookmarkx, {
+        foreignKey: "userId",
+      })
     }
   };
   Userx.init({
